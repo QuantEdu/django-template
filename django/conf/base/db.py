278b1330631e4c12a8f25e9507749887adcb80de
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-01-20 16:27:17 +0500

    Django split-settings structure

---
+++ django/conf/base/db.py
@@ -0,0 +1,16 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}

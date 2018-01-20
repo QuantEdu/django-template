75f688a3a2d62192657d01364721216d97c04dc6
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-01-20 16:56:58 +0500

    Logs and sites.app fixes

--- django/conf/base/apps.py
+++ django/conf/base/apps.py
@@ -4,6 +4,5 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.sites',
     'django.contrib.staticfiles'
 ]
--- django/conf/base/logging.py
+++ django/conf/base/logging.py
@@ -5,16 +5,23 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    # What happens with to ech message in loggers
     'handlers': {
-        'file': {
+        'file_all': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': os.path.join(BASE_DIR, "logs/debug.log"),
         },
+        'file_errors': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, "logs/error.log"),
+        }
     },
+    # Place for store all messages
     'loggers': {
         'django': {
-            'handlers': ['file'],
+            'handlers': ['file_all', 'file_errors'],
             'level': 'DEBUG',
             'propagate': True,
         },

ea6a644c2369fa4916dd624a4b12365c4c43cc71
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-01-20 17:05:22 +0500

    Logging process fix

--- django/conf/base/logging.py
+++ django/conf/base/logging.py
@@ -11,11 +11,18 @@ LOGGING = {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': os.path.join(BASE_DIR, "logs/debug.log"),
+            'formatter': 'simple'
         },
         'file_errors': {
             'level': 'ERROR',
             'class': 'logging.FileHandler',
             'filename': os.path.join(BASE_DIR, "logs/error.log"),
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'simple'
         }
     },
     # Place for store all messages
@@ -23,7 +30,15 @@ LOGGING = {
         'django': {
             'handlers': ['file_all', 'file_errors'],
             'level': 'DEBUG',
-            'propagate': True,
+            'propagate': True
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
         },
     },
 }

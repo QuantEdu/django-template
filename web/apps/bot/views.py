8046b5fbfb5326f8a92b8dad2730e6ab41fc498b
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-04-30 21:17:35 +0500

    bot confirmation

--- web/apps/bot/views.py
+++ web/apps/bot/views.py
@@ -10,7 +10,7 @@ def callback(request):
     if request.method == 'POST':
         # data = json.loads(request.data)
         # print(data)
-        print('Request:'.format(request.POST))
+        print('Request:'.format(json.loads(request.body)))
         # if 'type' not in data.keys():
         #     return 'not vk'
         # if data['type'] == 'confirmation':

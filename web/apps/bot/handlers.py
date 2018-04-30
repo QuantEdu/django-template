ebd4c3c8020c909405a6eecfa5ee32f459ef8668
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-04-30 23:26:39 +0500

    backfix

--- web/apps/bot/handlers.py
+++ web/apps/bot/handlers.py
@@ -1,24 +1,11 @@
-from . import vkapi
-import importlib, os
-from . import commands
+import vkapi
 
 
 def get_answer(body):
-   message = 'Привет, я новый бот! Не шли мне больше {}'.format(body)
+   message = 'Привет, я новый бот! Не шли мне больше {}'.format(data['object']['body'])
    return message
 
-def get_answer(body):
-    # Сообщение по умолчанию если распознать не удастся
-    message = "Прости, не понимаю тебя. Напиши 'помощь', чтобы узнать мои команды"
-    attachment = ''
-    print(commands.command_list)
-    for c in commands.command_list:
-        if body in c.keys:
-            message, attachment = c.process()
-    return message, attachment
-
 def create_answer(data, token):
-   from .commands import hello, info
    user_id = data['user_id']
-   message, attachment = get_answer(data['body'].lower())
-   vkapi.send_message(user_id, token, message, attachment)
+   message = get_answer(data['body'].lower())
+   vkapi.send_message(user_id, token, message)

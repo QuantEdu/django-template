3e7ba771ffba606704c5e21bb19b8a527b616b9f
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-04-30 20:49:55 +0500

    bot confirmation

--- web/apps/bot/views.py
+++ web/apps/bot/views.py
@@ -8,14 +8,14 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
-        data = json.loads(request.data)
-        print(data)
+        # data = json.loads(request.data)
+        # print(data)
         print(request)
-        if 'type' not in data.keys():
-            return 'not vk'
-        if data['type'] == 'confirmation':
-            return 'b609300d'
-        else:
-            return HttpResponse('ok')
+        # if 'type' not in data.keys():
+        #     return 'not vk'
+        # if data['type'] == 'confirmation':
+        #     return 'b609300d'
+        # else:
+        #     return HttpResponse('ok')
     else:
         return HttpResponse('not post')

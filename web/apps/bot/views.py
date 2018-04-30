72f14e2240a581e21c5730ffc2f3c98ccfe85400
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-04-30 18:05:04 +0500

    bot confirmation

--- web/apps/bot/views.py
+++ web/apps/bot/views.py
@@ -1,8 +1,15 @@
 # Django core
+import json
 from django.http import HttpResponse
 
 
 # Url, where user redirects after success vk authorization
 def callback(request):
-    print(request.body)
-    return HttpResponse('ok')
+    data = json.loads(request.data)
+    print(data)
+    if 'type' not in data.keys():
+        return 'not vk'
+    if data['type'] == 'confirmation':
+        return 'b609300d'
+    else:
+        return HttpResponse('ok')

271c5679c52f2c8d514697c87600d0c651703643
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-04-30 23:20:23 +0500

    refactor

--- web/apps/bot/handlers.py
+++ web/apps/bot/handlers.py
@@ -1,13 +1,23 @@
 from . import vkapi
 import importlib, os
+from . import commands
 
 
 def get_answer(body):
    message = 'Привет, я новый бот! Не шли мне больше {}'.format(body)
    return message
 
+def get_answer(body):
+    # Сообщение по умолчанию если распознать не удастся
+    message = "Прости, не понимаю тебя. Напиши 'помощь', чтобы узнать мои команды"
+    attachment = ''
+    for c in commands.command_list:
+        if body in c.keys:
+            message, attachment = c.process()
+    return message, attachment
+
 def create_answer(data, token):
    from .commands import hello, info
    user_id = data['user_id']
-   message = get_answer(data['body'].lower())
-   vkapi.send_message(user_id, token, message)
+   message, attachment = get_answer(data['body'].lower())
+   vkapi.send_message(user_id, token, message, attachment)

2b106946d84ec4a0c10eec4da913969f8ef26885
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-05-01 09:16:16 +0500

    try attachment

--- web/apps/bot/handlers.py
+++ web/apps/bot/handlers.py
@@ -2,10 +2,17 @@ from . import vkapi
 
 
 def create_answer(data, token):
-   user_id = data['user_id']
-   body = data['body'].lower()
-   if body in ['help', 'помощь']:
-       message = 'Привет, я новый бот! Ты выбрал команду help'
-   else:
-       message = 'Привет, я новый бот! Я тебя не понял. Поэтому не шли мне больше {}'.format(body)
-   vkapi.send_message(user_id, token, message)
+    user_id = data['user_id']
+    body = data['body'].lower()
+
+    # Default values
+    message, attachment = 'Непонятно', ''
+
+    if body in ['help', 'помощь']:
+        message = 'Привет, я новый бот! Ты выбрал команду help'
+    if body in ['задача']:
+        message = 'Привет, я новый бот! Ты выбрал команду help'
+        attachment = 'photo-165396328_456239018'
+    else:
+        message = 'Привет, я новый бот! Я тебя не понял. Поэтому не шли мне больше {}'.format(body)
+    vkapi.send_message(user_id, token, message, attachment)

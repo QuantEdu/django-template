562b85ed5d03f5b06edc954127095ac0cd2bacbc
ekaterinaparamonova <teleport19@gmail.com>
2018-06-27 16:40:21 +0500

    prints to vkapi

--- web/apps/bot/vkapi.py
+++ web/apps/bot/vkapi.py
@@ -6,6 +6,7 @@ api = vk.API(session, v=5.78)
 
 def send_message(user_id, token, message, attachment="", keyboard=""):
     print('vkapi.py send_message')
-    api.messages.send(access_token=token, user_id=str(user_id),
+    result = api.messages.send(access_token=token, user_id=str(user_id),
                       message=message, attachment=attachment, keyboard=keyboard)
+    print(result)
 

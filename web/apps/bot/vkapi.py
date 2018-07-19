8c865d33baf362fd683aa6a75c84f9df85614d60
ekaterinaparamonova <teleport19@gmail.com>
2018-07-19 20:12:30 +0500

    add keyboard parameter to message.send

--- web/apps/bot/vkapi.py
+++ web/apps/bot/vkapi.py
@@ -8,9 +8,11 @@ def send_message(user_id, token, message, attachment, keyboard):
     print('vkapi.py send_message')
     try:
         if keyboard is None:
-            result = api.messages.send(access_token=token, user_id=str(user_id),
-                      message=message, attachment=attachment)
-            print(result)
+            api.messages.send(access_token=token, user_id=str(user_id),
+                    message=message, attachment=attachment)
+        else:
+            api.messages.send(access_token=token, user_id=str(user_id),
+                    message=message, attachment=attachment, keyboard=keyboard)
     except Exception as e:
         print(e)
 

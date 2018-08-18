f5f7c9f153d4f4a0d84161be3240917a211ab6a8
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-08-18 12:31:56 +0500

    form works

--- web/apps/lp/views.py
+++ web/apps/lp/views.py
@@ -1,8 +1,19 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from .forms import LeadForm
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'lp/index.html'
+    form_class = LeadForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.create_lead(form)
+        return super().form_valid(form)
 
 
 class NumIndexView(TemplateView):

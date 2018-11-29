71a3c1657f22871e7c417c9fb5cf044d8ac9ebca
ekaterinaparamonova <teleport19@gmail.com>
2018-11-29 15:57:32 +0500

    add tasks app model

---
+++ web/apps/lms/tasks/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+from apps.crm.users import User
+from apps.lms.lessons.models import Lesson
+
+# Main model for tasks
+class Task(models.Model):
+
+    lesson = models.ForeignKey(Lesson, verbose_name="Урок", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    deadline = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'задание по уроку'
+        verbose_name_plural = 'задания по уроку'
+
+    def __str__(self):
+        return u'{}, {}'.format(self.user, self.lesson)
+
+
--- web/apps/results/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResultsConfig(AppConfig):
-    name = "apps.results"
-    verbose_name = "Результаты"

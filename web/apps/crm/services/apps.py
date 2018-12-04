58142a6fb058306bcee064c339e6a88f2ed4f20c
ekaterinaparamonova <teleport19@gmail.com>
2018-12-04 23:04:42 +0500

    bud fixes

--- web/apps/crm/services/apps.py
+++ web/apps/crm/services/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ServicesConfig(AppConfig):
-    name = "apps.services"
+    name = "apps.crm.services"
     verbose_name = "Услуги"
--- web/apps/crm/social/apps.py
+++ web/apps/crm/social/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SocialConfig(AppConfig):
-    name = "apps.social"
+    name = "apps.crm.social"
     verbose_name = "Социальные сети"
